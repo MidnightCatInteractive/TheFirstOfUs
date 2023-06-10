40f48a650612bc4f5ca27b2c4f94f857951de0d5
Roko Fumić <60196246+rfumic@users.noreply.github.com>
2023-06-10 18:10:38 +0200

    Added stats UI and implemented hunger/water

--- Assets/Scripts/CharacterScripts/PlayerStatusScript.cs
+++ Assets/Scripts/CharacterScripts/PlayerStatusScript.cs
@@ -9,13 +9,50 @@ public class PlayerStatusScript : MonoBehaviour
     public float currentWater = 100;
     public float currentHealth = 100;
 
+    public float hungerInterval = 1f;
+    public float hungerDecreaseAmount = 1;
+    public float waterInterval = 1f;
+    public float waterDecreaseAmount = 1;
+    private float dehydration;
+    
+    private float hungerTimer;
+    private float waterTimer;
+
     void Start()
     {
-       
+      hungerTimer = hungerInterval;
+      waterTimer = waterInterval; 
     }
 
     void Update()
     {
+        hungerTimer -= Time.deltaTime;
+        waterTimer -= Time.deltaTime;
+
+        currentWater = (currentWater < 0) ? 0 : currentWater;
+        dehydration = (currentWater <= 0) ? 50 : 1;
+
+        if(hungerTimer <= 0f)
+        {
+           DecreaseStat(ref currentHealth, hungerDecreaseAmount);
+           hungerTimer = hungerInterval;
+        }
+ 
+        if(waterTimer <= 0f)
+        {
+           DecreaseStat(ref currentWater, waterDecreaseAmount * dehydration);
+           waterTimer = waterInterval;
+        }
+
+        if(currentHealth <=0)
+        {
+            Debug.Log("Player is dead");
+        }
         
+    }
+
+    void DecreaseStat(ref float stat, float amount)
+    {
+        stat -= amount;
     }
 }
